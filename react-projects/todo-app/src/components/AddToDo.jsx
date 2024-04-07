a74a3cf817fc8c75564409a4d72969357a065e97
Soundaryas123 <soundaryas123@gmail.com>
2024-04-07 20:13:00 +0530

    todo updated using useref

--- react-projects/todo-app/src/components/AddToDo.jsx
+++ react-projects/todo-app/src/components/AddToDo.jsx
@@ -1,51 +1,44 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/AddToDo.module.css";
 import { GrTableAdd } from "react-icons/gr";
 
 function AddToDo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
+  const todoNameElement = useRef();
+  const dueDateNameElement = useRef();
 
-  const handleAddNewItem = () => {
-    event.preventDefault()
+  const handleAddNewItem = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateNameElement.current.value;
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
+    todoNameElement.current.value = "";
+    dueDateNameElement.current.value = "";
   };
 
   return (
     <div className={`container text-center ${styles["container-margin"]}`}>
-      <form className="row"  onSubmit={handleAddNewItem}>
+      <form className="row" onSubmit={handleAddNewItem}>
         <div className="col-6">
           <input
             type="text"
-            value={todoName}
             placeholder="Enter Todo here"
             className={styles["field"]}
-            onChange={handleNameChange}
+            ref={todoNameElement}
           />
         </div>
         <div className="col-4">
           <input
             type="date"
-            value={dueDate}
             className={styles["field"]}
-            onChange={handleDateChange}
+            ref={dueDateNameElement}
           />
         </div>
         <div className="col-2">
           <button
-          type="submit"
+            type="submit"
             className={`btn btn-success ${styles["btn-width"]}`}
-           
           >
-       <GrTableAdd />
+            <GrTableAdd />
           </button>
         </div>
       </form>
